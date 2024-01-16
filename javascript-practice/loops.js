401cdb23524afdc17125bebd096eabf55d189ae4
srilakshmi kongala <kongalasriakshmi@gmail.com>
2024-01-16 10:18:00 +0530

    loops

---
+++ javascript-practice/loops.js
@@ -0,0 +1,26 @@
+
+// Example of a for loop that iterates from 1 to 5
+for (let i = 1; i <= 5; i++) {
+    console.log("Iteration:", i);
+  }
+
+  
+  // Example of a while loop that iterates as long as a condition is true
+let counter = 1;
+while (counter <= 5) {
+  console.log("Iteration:", counter);
+  counter++;
+}
+
+
+
+// Example of a do-while loop that executes at least once and continues as long as a condition is true
+let number = 1;
+do {
+  console.log("Number:", number);
+  number++;
+} while (number <= 5);
+
+
+
+

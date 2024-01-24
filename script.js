24e29e905b649e6b0039c2597b64052bf516d9d7
srilakshmi kongala <kongalasriakshmi@gmail.com>
2024-01-24 22:55:03 +0530

    if
    Signed-off-by: srilakshmi kongala <kongalasriakshmi@gmail.com>

---
+++ script.js
@@ -0,0 +1,11 @@
+// check if the number is positive
+
+const number = prompt("Enter a number: ");
+
+// check if number is greater than 0
+if (number > 0) {
+
+  console.log("The number is positive");
+}
+
+console.log("The if statement is easy");
